88d2eb897543071abb8a8a5f8201c25aabc6e764
OMSAVITHRU <THATHSAVITHUR@GMAIL.COM>
2026-08-30 11:54:02 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-@app.route("/webhook", methods=["GET"])
-def verify():
-    print(f"ALL ARGS: {dict(request.args)}")
-    challenge = request.args.get("hub.challenge")
-    if challenge:
-        return challenge, 200
-    return "OK", 200
